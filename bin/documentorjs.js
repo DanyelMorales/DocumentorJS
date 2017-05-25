b22e10499d7d3f6be961be140c991df11589c648
Daniel Morales <Daniel Morales>
2017-05-25 00:46:13 -0500

    Feat: Normalizar las rutas si existe un CWD
    Si se pasa un CWD de forma explicita se debera normalizar la ruta de los
    archivos a observar y utilizarse para el APIDOC.

--- bin/documentorjs.js
+++ bin/documentorjs.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-
 var grunt = require('grunt');
 var fs = require('fs');
 const path = require('path');
-
+ 
 var configFile  = "/apidoc.config.js";
 var workingdir = path.join(process.cwd(),configFile);
+var gruntFile =  path.join(__dirname , "/../Gruntfile.js");
 
 // verificando si existe el archivo de configuracion
-if (!fs.existsSync(workingdir)) {
+if (!fs.existsSync(workingdir) || !fs.existsSync(gruntFile)) {
 	console.log("NOT FOUND MAIN CONFIG:" + workingdir);
    return new Error(workingdir);
 }
 
 // Lanzando el watcher
-grunt.tasks(['watch']);
+
 grunt.cli({
-  gruntfile: __dirname + "/../Gruntfile.js",
-  extra: {"configFile": workingdir, "invokedFrom":process.cwd()}
+  gruntfile: gruntFile,
+  extra: {"configFile": workingdir, "invokedFrom":process.cwd(), "packagePath":path.join(__dirname , "/../package.json")}
 });
---
+++ bin/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash 
+cd .. ; 
+npm install apidoc -g;
+npm install grunt -g ;
+npm install grunt-cli -g;
+npm install grunt-contrib-watch --save-dev;
+npm install grunt-exec --save-dev ;
+npm link 
