221035d93b2896454c8e7a28fbba9e65b99808b7
Daniel Vera Morales <dvera@sunset.com.mx>
2018-04-24 11:42:33 -0500

    Refact project name

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash 
+#!/usr/bin/env bash 
 cd .. ; 
-npm install apidoc -g;
-npm install grunt -g ;
-npm install grunt-cli -g;
-npm install grunt-contrib-watch --save-dev;
-npm install grunt-exec --save-dev ;
-npm install grunt-apidoc --save-dev;
+npm install apidoc -g &&
+npm install grunt -g &&
+npm install grunt-cli -g &&
+npm install grunt-contrib-watch --save-dev &&
+npm install grunt-exec --save-dev &&
+npm install grunt-apidoc --save-dev&&
 npm link 
